353f3d7357e87571e7aa6c4d47f34694120e16bc
Júlíus Guðni <julius@ext.is>
2021-12-28 17:08:49 +0000

    Update actions.py

--- browser/microsoft-edge-dev/actions.py
+++ browser/microsoft-edge-dev/actions.py
@@ -12,7 +12,7 @@ Suffix = "-1"
 
 def setup():
     shelltools.system("pwd")
-    shelltools.system("ar xf microsoft-edge-dev_%s-1_amd64.deb" % (get.srcVERSION()))
+    shelltools.system("ar xf microsoft-edge-stable_96_%s-1_amd64.deb" % (get.srcVERSION()))
     shelltools.system("tar xvf data.tar.xz")
     shelltools.system("ln -s /opt/microsoft/msedge-dev/product_logo_128_dev.png /usr/share/pixmaps/microsoft-edge-dev.png")
 
